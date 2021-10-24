086fa243f3878e9b4166f61df53362c47b18f1fe
eCatarino <eduardo.ctrm@gmail.com>
2021-10-24 21:09:50 +0100

    Update streamingtest.js

--- js/streamingtest.js
+++ js/streamingtest.js
@@ -45,9 +45,9 @@
 // window.location.hostname
 var server = null;
 if(window.location.protocol === 'http:')
-	server = "http://" + window.location.hostname + ":8088/janus";
+	server = "http://" + window.location.hostname + ":8088/janus-gateway/janus";
 else
-	server = "https://" + window.location.hostname + ":8089/janus";
+	server = "https://" + window.location.hostname + ":8089/janus-gateway/janus";
 
 var janus = null;
 var streaming = null;
